bb980c7ff07b44b1c6519431ddbda5c2b641badb
Mainul Hasan <mainulhasan7070@gmail.com>
2023-08-16 01:41:16 +0600

    fix: autofocous in formst

--- task/forms.py
+++ task/forms.py
@@ -6,8 +6,7 @@ from task.models import TaskStoreModel
 class TaskStoreForm(forms.ModelForm):
     task = forms.CharField(max_length=50, widget=forms.TextInput(
         attrs={'autofocus': 'autofocus'}))
-    detail = forms.CharField(max_length=50, required=False, widget=forms.TextInput(
-        attrs={'autofocus': 'autofocus'}))
+    detail = forms.CharField(max_length=250, required=False)
 
     class Meta:
         model = TaskStoreModel
